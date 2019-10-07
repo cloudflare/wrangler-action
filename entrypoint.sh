83ae7e5d0910ffeab8a91031a0215f2e93dd65af
Kristian Freeman <kristian@kristianfreeman.com>
2019-10-07 15:38:18 -0500

    Install curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+apt install update
+apt install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | sh
 nvm install latest
 npm i
 npm i @cloudflare/wrangler -g
