698bc9db775884e1c6276cade1e3dcd5e72b99e1
Kristian Freeman <kristian@kristianfreeman.com>
2019-10-11 14:34:18 -0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ export NVM_DIR="/github/workspace/nvm"
 export WRANGLER_HOME="/github/workspace"
 
 # h/t https://github.com/elgohr/Publish-Docker-Github-Action
-function sanitize() {
+function sanitize () {
   if [ -z "${1}" ]; then
     >&2 echo "Unable to find the ${2}. Did you set with.${2}?"
     exit 1
@@ -21,9 +21,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 mkdir -p "$HOME/.wrangler"
 chmod -R 777 "$HOME/.wrangler"
 
-_sanitize "${INPUT_EMAIL}" "email"
+sanitize "${INPUT_EMAIL}" "email"
 export CLOUDFLARE_EMAIL="$INPUT_EMAIL"
-_sanitize "${INPUT_APIKEY}" "apiKey"
+sanitize "${INPUT_APIKEY}" "apiKey"
 export CLOUDFLARE_API_KEY="$INPUT_APIKEY"
 
 npm i @cloudflare/wrangler -g
