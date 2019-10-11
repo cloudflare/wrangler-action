4f3696e97859b0bc3ffd8aca6c497e5265898cb0
Kristian Freeman <kristian@kristianfreeman.com>
2019-10-11 12:32:05 -0500

    Check for env vars, correct workspace dir

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM node:10.14.1-alpine
 COPY entrypoint.sh /entrypoint.sh
+ENV WRANGLER_HOME /github/workspace
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+if [ -n "$CLOUDFLARE_API_KEY" ]; then
+  echo "CLOUDFLARE_API_KEY env var needs to be set. Add this field in the 'Secrets' section of your repo's settings."
+  exit 1
+fi
+
+if [ -n "$CLOUDFLARE_EMAIL" ]; then
+  echo "CLOUDFLARE_EMAIL env var needs to be set. Add this field in the 'Secrets' section of your repo's settings."
+  exit 1
+fi
+
 npm i
 npm i @cloudflare/wrangler -g
 wrangler whoami
