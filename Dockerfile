32bb6468e77dd9bac520f100832487b59f9af2ea
Kristian Freeman <kristian@kristianfreeman.com>
2019-10-11 13:01:57 -0500

    Updates from testing locally

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM node:12
 ENV XDG_CONFIG_HOME /github/workspace
-RUN "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash"
-RUN "nvm install latest"
 ENV WRANGLER_HOME /github/workspace
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+export HOME="/github/workspace"
+export NVM_DIR="/github/workspace/nvm"
+export WRANGLER_HOME="/github/workspace"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 if [ -n "$CLOUDFLARE_API_KEY" ]; then
   echo "CLOUDFLARE_API_KEY env var needs to be set. Add this field in the 'Secrets' section of your repo's settings."
   exit 1
@@ -12,7 +20,8 @@ if [ -n "$CLOUDFLARE_EMAIL" ]; then
   exit 1
 fi
 
-npm i
 npm i @cloudflare/wrangler -g
+npm i
+
 wrangler whoami
 wrangler publish
