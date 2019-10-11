d7880225502cd63b9620d9629719a54f46e80827
Kristian Freeman <kristian@kristianfreeman.com>
2019-10-11 12:22:20 -0500

    Use node docker image

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM ubuntu:latest
+FROM node:10.14.1-alpine
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/sh -l
+#!/bin/sh
 
-apt-get update -y
-apt-get install curl -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | sh
-nvm install latest
+set -e
+
 npm i
 npm i @cloudflare/wrangler -g
 wrangler whoami
