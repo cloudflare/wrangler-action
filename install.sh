43b1736eab4144b84474e0bf25b257a3caf93ecb
alank <alankan976@gmail.com>
2020-02-04 07:59:35 +0800

    add bash -x for tracing

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 export HOME="/github/workspace"
 export NVM_DIR="/github/workspace/nvm"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash -x
 # Comments beginning with "shellcheck" use shellcheck, a shell script linter.
 # The below comments ignore shellcheck linting on the instructions provided
 # by NVM.
