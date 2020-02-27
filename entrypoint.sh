1202bb2fe9ffbc61d8e0cf5476cde62a90ab14fa
Jason Gill <jasongill@gmail.com>
2020-02-27 13:42:05 -0500

    Remove nvm to speed build time
    The Dockerfile already pulls the node:12 image, so there's no need for NVM to be downloaded and installed as well. Removing this speeds up the image build time (making Github Actions go faster)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,7 @@
 set -e
 
 export HOME="/github/workspace"
-export NVM_DIR="/github/workspace/nvm"
 export WRANGLER_HOME="/github/workspace"
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
-
-# Comments beginning with "shellcheck" use shellcheck, a shell script linter.
-# The below comments ignore shellcheck linting on the instructions provided
-# by NVM.
-
-# shellcheck source=/dev/null
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# shellcheck source=/dev/null
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 mkdir -p "$HOME/.wrangler"
 chmod -R 770 "$HOME/.wrangler"
