40059cb7198bacf1340be07b08391effa645277b
Webber <webber@takken.io>
2021-06-11 22:08:43 +0200

    Add configFile option

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,10 +79,23 @@ then
   cd "$INPUT_WORKINGDIRECTORY"
 fi
 
-# If precommands is detected as input
+# If preCommands is detected as input
 if [ -n "$INPUT_PRECOMMANDS" ]
 then
   execute_commands "$INPUT_PRECOMMANDS"
+fi
+
+# Flags that are passed to the wrangler command
+export FLAGS=""
+
+# Environment was specified
+if [ -n "$INPUT_ENVIRONMENT" ] ; then
+  export FLAGS="$FLAGS --env $INPUT_ENVIRONMENT"
+fi
+
+# A non-default configuration file was specified
+if [ -n "$INPUT_CONFIGFILE" ] ; then
+  export FLAGS="$FLAGS --config $INPUT_CONFIGFILE"
 fi
 
 # If an environment is detected as input, for each secret specified get the value of
@@ -90,25 +103,15 @@ fi
 # Skip if publish is set to false.
 if [ "$INPUT_PUBLISH" != "false" ]
 then
-  if [ -z "$INPUT_ENVIRONMENT" ]
-  then
-    wrangler publish
+  wrangler publish "$FLAGS"
 
-    for SECRET in $INPUT_SECRETS; do
-      VALUE=$(printenv "$SECRET") || secret_not_found "$SECRET"
-      echo "$VALUE" | wrangler secret put "$SECRET"
-    done
-  else
-    wrangler publish -e "$INPUT_ENVIRONMENT"
-
-    for SECRET in $INPUT_SECRETS; do
-      VALUE=$(printenv "$SECRET") || secret_not_found "$SECRET"
-      echo "$VALUE" | wrangler secret put "$SECRET" --env "$INPUT_ENVIRONMENT"
-    done
-  fi
+  for SECRET in $INPUT_SECRETS; do
+    VALUE=$(printenv "$SECRET") || secret_not_found "$SECRET"
+    echo "$VALUE" | wrangler secret put "$SECRET" "$FLAGS"
+  done
 fi
 
-# If postcommands is detected as input
+# If postCommands is detected as input
 if [ -n "$INPUT_POSTCOMMANDS" ]
 then
   execute_commands "$INPUT_POSTCOMMANDS"
