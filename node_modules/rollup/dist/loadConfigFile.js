20ea5060497220b70723b2c5fdf847822eaaa7fa
Jacob M-G Evans <jacobmgevans@gmail.com>
2023-08-07 15:11:15 -0500

    Temp fix for deps issue

---
+++ node_modules/rollup/dist/loadConfigFile.js
@@ -0,0 +1,29 @@
+/*
+  @license
+	Rollup.js v3.27.0
+	Fri, 28 Jul 2023 14:16:41 GMT - commit ac502f2dbdd2d96cb55547fd94942f170978fefd
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+'use strict';
+
+Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+
+require('node:fs/promises');
+require('node:path');
+require('node:process');
+require('node:url');
+require('./shared/rollup.js');
+const loadConfigFile_js = require('./shared/loadConfigFile.js');
+require('tty');
+require('path');
+require('node:perf_hooks');
+require('node:crypto');
+require('./getLogFilter.js');
+
+
+
+exports.loadConfigFile = loadConfigFile_js.loadConfigFile;
+//# sourceMappingURL=loadConfigFile.js.map
---
+++ node_modules/rollup/dist/shared/fsevents-importer.js
@@ -0,0 +1,37 @@
+/*
+  @license
+	Rollup.js v3.27.0
+	Fri, 28 Jul 2023 14:16:41 GMT - commit ac502f2dbdd2d96cb55547fd94942f170978fefd
+
+	https://github.com/rollup/rollup
+
+	Released under the MIT License.
+*/
+'use strict';
+
+let fsEvents;
+let fsEventsImportError;
+async function loadFsEvents() {
+    try {
+        ({ default: fsEvents } = await import('fsevents'));
+    }
+    catch (error) {
+        fsEventsImportError = error;
+    }
+}
+// A call to this function will be injected into the chokidar code
+function getFsEvents() {
+    if (fsEventsImportError)
+        throw fsEventsImportError;
+    return fsEvents;
+}
+
+const fseventsImporter = /*#__PURE__*/Object.defineProperty({
+    __proto__: null,
+    getFsEvents,
+    loadFsEvents
+}, Symbol.toStringTag, { value: 'Module' });
+
+exports.fseventsImporter = fseventsImporter;
+exports.loadFsEvents = loadFsEvents;
+//# sourceMappingURL=fsevents-importer.js.map
