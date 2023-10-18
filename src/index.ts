473d9cbd296528b41c653af10062faba6540a7ab
Cina Saffary <cina@cloudflare.com>
2023-10-18 17:04:41 -0500

    Bump `DEFAULT_WRANGLER_VERSION` to 3.13.2

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { exec, execShell } from "./exec";
 import { checkWorkingDirectory, semverCompare } from "./utils";
 import { getPackageManager } from "./packageManagers";
 
-const DEFAULT_WRANGLER_VERSION = "3.5.1";
+const DEFAULT_WRANGLER_VERSION = "3.13.2";
 
 /**
  * A configuration object that contains all the inputs & immutable state for the action.
