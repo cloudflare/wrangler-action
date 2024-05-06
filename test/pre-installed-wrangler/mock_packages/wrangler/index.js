afaec621046f20655ab054772ec8499197b417bb
Adishwar Rishi <adiswa123@gmail.com>
2024-05-06 18:00:10 +1000

    fix(test): Fix execution for fake wrangler installation

--- test/pre-installed-wrangler/mock_packages/wrangler/index.js
+++ test/pre-installed-wrangler/mock_packages/wrangler/index.js
@@ -1,10 +1,13 @@
+#!/usr/bin/env node
+"use strict";
+
 const args = Array.from(process.argv);
 const command = args.pop();
 switch (command) {
 	case "--version":
-		console.log(
-			"⛅️ wrangler 1.1.1 (update available 1.2.3)\n" +
-				"-------------------------------------------------------",
+		console.log(`
+⛅️ wrangler 1.1.1 (update available 1.2.3)
+------------------------------------------`
 		);
 		process.exit(0);
 	case "action-test":
