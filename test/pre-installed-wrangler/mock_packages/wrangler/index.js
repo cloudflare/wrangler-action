186b64fad0466b597c693a559531c1c0fae6d63f
Adishwar Rishi <adiswa123@gmail.com>
2024-06-09 13:20:20 +1000

    Revert "Revert "(feat): Use existing wrangler installation when appropriate (#235)""
    This reverts commit 2d275a8f2d279dc91912c1ff8023af109ef3280c.

---
+++ test/pre-installed-wrangler/mock_packages/wrangler/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+"use strict";
+
+const args = Array.from(process.argv);
+const command = args.pop();
+switch (command) {
+	case "--version":
+		console.log(`
+⛅️ wrangler 1.1.1 (update available 1.2.3)
+------------------------------------------`);
+		process.exit(0);
+	case "action-test":
+		console.log("Test successful.");
+		process.exit(0);
+	default:
+		console.error("Invalid command");
+		process.exit(1);
+}
