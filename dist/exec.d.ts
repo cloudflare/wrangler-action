84d8a05b2c305df55219f117ceeac9db0d3b311a
Courtney Sims <csims@cloudflare.com>
2024-10-08 16:17:45 -0500

    adding compiled files for testing

---
+++ dist/exec.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+import { exec as _childProcessExec } from "node:child_process";
+export { exec } from "@actions/exec";
+declare const childProcessExec: typeof _childProcessExec.__promisify__;
+export declare function execShell(command: string, { silent, ...options }?: Parameters<typeof childProcessExec>[1] & {
+    silent?: boolean;
+}): Promise<number | null>;
---
+++ dist/utils.d.ts
@@ -0,0 +1,5 @@
+/**
+ * A helper function to compare two semver versions. If the second arg is greater than the first arg, it returns true.
+ */
+export declare function semverCompare(version1: string, version2: string): boolean;
+export declare function checkWorkingDirectory(workingDirectory?: string): string;
