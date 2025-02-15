72094723eadd23a44947f774c96596305a640c45
Iiro Jäppinen <iiro@jappinen.fi>
2025-02-15 14:08:03 +0200

    feat: add `version-id` to action output of `wrangler versions upload` command

--- src/wranglerArtifactManager.ts
+++ src/wranglerArtifactManager.ts
@@ -45,6 +45,8 @@ export type OutputEntryVersionUpload = z.infer<typeof OutputEntryVersionUpload>;
 const OutputEntryVersionUpload = OutputEntryBase.merge(
 	z.object({
 		type: z.literal("version-upload"),
+		/** The id of this uploaded version. Format is UUID . */
+		version_id: z.string().optional(),
 		/** The preview URL associated with this version upload */
 		preview_url: z.string().optional(),
 	}),
