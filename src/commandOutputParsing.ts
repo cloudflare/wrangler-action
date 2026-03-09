904f77cab24cc4500077ece0dfe72b18fac5067b
Dario Piotrowicz <dario@cloudflare.com>
2026-03-09 01:37:40 +0000

    fix formatting

--- src/commandOutputParsing.ts
+++ src/commandOutputParsing.ts
@@ -113,7 +113,7 @@ function handleVersionsUploadOutputEntry(
 ) {
 	setOutput("deployment-url", versionsOutputEntry.preview_url);
 	setOutput("deployment-alias-url", versionsOutputEntry.preview_alias_url);
-	setOutput('version-id', versionsOutputEntry.version_id);
+	setOutput("version-id", versionsOutputEntry.version_id);
 }
 
 /**
